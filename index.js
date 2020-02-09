6bd1e437e2bf36b49e8c20e2e18934c2faa45959
Justin <me@justinlam.ca>
2020-02-09 03:02:19 -0500

    Add more usage validation;

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var Path = require('path');
 
 var yargs = require('yargs');
+
+var Validation = require('./lib/models/validation');
+var FileUtil = require('./lib/utils/file');
 var ParseController = require('./lib/controllers/parser');
+var ConfigController = require('./lib/controllers/config');
 
 yargs.command('transform <source> [destination]','Transform your flat delimited flat files', function(yargs){
   yargs.positional('source', {
@@ -13,11 +17,25 @@ yargs.command('transform <source> [destination]','Transform your flat delimited 
     default: __dirname
   });
 }, function(argv){
-  var filename = Path.basename(argv.source);
-  var filenameSplit = filename.split('.');
-  filename = filenameSplit[0]+'-autobot-'+Date.now()+'.'+filenameSplit[1];
-  ParseController.parse(argv.source, argv.destination+'/'+filename);
+  var validation = new Validation();
+  FileUtil.checkFile(argv.source).then(function(){
+    return FileUtil.checkDirectory(argv.destination, true);
+  }).then(function(){
+    console.log('> config', argv.config);
+    var filename = Path.basename(argv.source);
+    var filenameSplit = filename.split('.');
+    filename = filenameSplit[0]+'-autobot-'+Date.now()+'.'+filenameSplit[1];
+    
+    // ParseController.parse(argv.source, argv.destination+'/'+filename);
+  }).catch(function(e){
+    console.log('> ',e);
+  });
 });
+
+yargs.option('config', {
+  alias: 'c',
+  describe: 'Path to configuration file.  If none is provided the current working directory'
+})
 
 
 yargs.help().argv;
